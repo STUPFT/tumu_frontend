fc47ee3e33004c5141d301ef6a3b8b58fe840ae5
csxiaocy <xiaochuyu@outlook.com>
2019-10-18 16:52:09 +0800

    引入ant-design-vue

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="container">
+    <!-- 搜索栏 -->
+    <div id="search-bar">
+      <a-input-search
+        placeholder="input search text"
+        @search="onSearch"
+        enterButton="Search"
+        size="large"
+      />
+    </div>
+    <!-- 测试回到顶部按钮用 -->
+    <div style="height:5000px;"></div>
+    <!-- 回到顶部 -->
+    <a-back-top />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      search_keyword: ''
+    }
+  },
+  methods: {
+    onSearch(value) {
+      this.search_keyword = value
+    }
+  },
+}
+</script>
+
+<style scoped>
+#container {
+  margin: 0;
+  padding: 0;
+}
+
+#search-bar {
+  height: 100%;
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
