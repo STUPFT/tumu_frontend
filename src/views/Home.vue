b044f2defdda9fcb46c8157218ce70194b7fc9a9
csxiaocy <xiaochuyu@outlook.com>
2019-10-23 00:49:46 +0800

    添加列表样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,8 +9,123 @@
         size="large"
       />
     </div>
-    <!-- 测试回到顶部按钮用 -->
-    <div style="height:5000px;"></div>
+    <!-- 地区卡片列表 -->
+    <div class="region-cards">
+      <div class="region-card">
+        <img src="../assets/test.jpg" alt="">
+        <div class="region-name">名称</div>
+        <div class="region-desc">
+          <h1>简介</h1>
+          <p>
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+          </p>
+          <a-button type="danger">查看更多</a-button>
+        </div>
+      </div>
+      <div class="region-card">
+        <img src="../assets/test.jpg" alt="">
+        <div class="region-name">名称</div>
+        <div class="region-desc">
+          <h1>简介</h1>
+          <p>
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+          </p>
+          <a-button type="danger">查看更多</a-button>
+        </div>
+      </div>
+      <div class="region-card">
+        <img src="../assets/test.jpg" alt="">
+        <div class="region-name">名称</div>
+        <div class="region-desc">
+          <h1>简介</h1>
+          <p>
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+          </p>
+          <a-button type="danger">查看更多</a-button>
+        </div>
+      </div>
+      <div class="region-card">
+        <img src="../assets/test.jpg" alt="">
+        <div class="region-name">名称</div>
+        <div class="region-desc">
+          <h1>简介</h1>
+          <p>
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+          </p>
+          <a-button type="danger">查看更多</a-button>
+        </div>
+      </div>
+      <div class="region-card">
+        <img src="../assets/test.jpg" alt="">
+        <div class="region-name">名称</div>
+        <div class="region-desc">
+          <h1>简介</h1>
+          <p>
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+          </p>
+          <a-button type="danger">查看更多</a-button>
+        </div>
+      </div>
+      <div class="region-card">
+        <img src="../assets/test.jpg" alt="">
+        <div class="region-name">名称</div>
+        <div class="region-desc">
+          <h1>简介</h1>
+          <p>
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+          </p>
+          <a-button type="danger">查看更多</a-button>
+        </div>
+      </div>
+      <div class="region-card">
+        <img src="../assets/test.jpg" alt="">
+        <div class="region-name">名称</div>
+        <div class="region-desc">
+          <h1>简介</h1>
+          <p>
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+          </p>
+          <a-button type="danger">查看更多</a-button>
+        </div>
+      </div>
+      <div class="region-card">
+        <img src="../assets/test.jpg" alt="">
+        <div class="region-name">名称</div>
+        <div class="region-desc">
+          <h1>简介</h1>
+          <p>
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+            如果你有问题，没有其他地方可以转向，神秘而难以捉摸的罗伯特·麦考尔将为你寻求的治安法官提供帮助
+          </p>
+          <a-button type="danger">查看更多</a-button>
+        </div>
+      </div>
+    </div>
+    <a-pagination style="margin-bottom: 200px;" v-model="current" :total="50" />
     <!-- 回到顶部 -->
     <a-back-top />
   </div>
@@ -21,7 +136,8 @@ export default {
   name: 'Home',
   data() {
     return {
-      search_keyword: ''
+      search_keyword: '',
+      current: 2,
     }
   },
   methods: {
@@ -42,5 +158,92 @@ export default {
   height: 100%;
   width: 50%;
   margin: 0 auto;
+}
+
+.region-cards {
+  max-width: 1340px;
+  height: 1160px;
+  margin: 40px auto;
+  padding: auto;
+}
+
+.region-card {
+  width: 380px;
+  height: 300px;
+  float: left;
+  margin: 32px;
+  cursor: pointer;
+  border: 3px solid rgb(236, 240, 241);
+  border-radius: 8px;
+  box-shadow: 1px 2px 1px #888888;
+  transition: all .5s cubic-bezier(.8, .5, .2, 1.4);
+}
+
+.region-card img {
+  width: 100%;
+  height: 260px;
+  border-radius: 6px;
+  transition: all .5s cubic-bezier(.8, .5, .2, 1.4);
+}
+
+.region-card .region-name {
+  width: 100%;
+  height: 40px;
+  transition: all .5s cubic-bezier(.8, .5, .2, 1.4);
+  font-size: 18px;
+  line-height: 36px;
+  bottom: 0;
+  text-align: center;
+  background-color: rgb(236, 240, 241);
+  color: #000;
+  opacity: 0.3;
+}
+
+.region-desc {
+  position: absolute;
+  top:0px;
+  left:0px;
+  background-color: rgba(255,255,255,.7);
+  width:100%;
+  height:100%;
+  transition: all .7s ease-in-out;
+  padding: 20px;
+  box-sizing: border-box;
+  clip-path: circle(0% at 100% 100%);
+}
+
+.region-card:hover .region-desc {
+  left:0px;
+  transition: all .7s ease-in-out;
+  clip-path: circle(75%);
+}
+
+.region-card:hover {
+  transition: all .5s cubic-bezier(.8,.5,.2,1.4);
+  box-shadow: 0px 2px 3px rgba(0,0,0,.3);
+  transform: scale(.97);
+}
+
+.region-card:hover img, .card:hover .region-name {
+  transition: all .5s cubic-bezier(.8,.5,.2,1.4);
+  filter: blur(3px);
+}
+
+.region-card h1 {
+  color: #ff3838;
+  letter-spacing: 1px;
+  margin: 0px;
+}
+
+.region-card p {
+  line-height: 24px;
+  height: 70%;
+  opacity: 0.8;
+  padding: 8px 0 0 6px;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 7;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
